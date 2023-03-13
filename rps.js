1019fbb882d2fd3651c0d87a7ccfcf5f52909774
Oliver <oloco200792@gmail.com>
2023-03-13 11:05:53 -0400

    Add switch case for conditions

--- rps.js
+++ rps.js
@@ -7,9 +7,34 @@ function getComputerChoice() {
     return choices[randomChoice];
 }
 
-function realGame(computerChoices, playerChoices) {
-    let getPlayerChoice = prompt("Rock, Paper or Scissor?");
-    let playerChoice = getPlayerChoice.toLowerCase();
-    let letra = getPlayerChoice.charAt(0);
-    return letra.toUpperCase() + playerChoice.slice(1);
+function realGame(computerChoices) {
+    let getPlayerChoices = prompt("Rock, Paper or Scissor?");
+    let playerChoice = getPlayerChoices.toLowerCase();
+    let firstLetter = getPlayerChoices.charAt(0);
+    let realPlayerChoice = firstLetter.toUpperCase() + playerChoice.slice(1);
+    switch (true) {
+        case realPlayerChoice === "Rock" && computerChoices === "Paper":
+            console.log("You Lose! Paper Beats Rock");
+            break;
+        case realPlayerChoice === "Rock" && computerChoices === "Scissor":
+            console.log("You Win! Rock beats Scissor");
+            break;
+        case realPlayerChoice === "Paper" && computerChoices === "Scissor":
+            console.log("You Lose! Scicssor beats Paper");
+            break;
+        case realPlayerChoice === "Paper" && computerChoices === "Rock":
+            console.log("You Win! Paper beats Rock");
+            break;
+        case realPlayerChoice === "Scissor" && computerChoices === "Rock":
+            console.log("You Lose! Rock beats Scissor");
+            break;
+        case realPlayerChoice === "Scissor" && computerChoices === "Paper":
+            console.log("You Win! Scissor beats Paper");
+            break;
+        case realPlayerChoice === computerChoices:
+            console.log("It's a Tie! No one wins")
+            break;
+    }
 }
+
+realGame(getComputerChoice())
