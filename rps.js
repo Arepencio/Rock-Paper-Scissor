e41c935d85a32034a73a01cbd449e7490f022c42
Oliver <oloco200792@gmail.com>
2023-09-25 22:43:18 -0300

    add querySelectors to buttons

--- rps.js
+++ rps.js
@@ -1,47 +1,52 @@
-function getPlayerChoice() {
-    let getPlayerChoice = prompt("Round ${round} Rock, Paper or Scissor?");
-    let playerChoice = getPlayerChoice.toLowerCase();
-    let firstLetter = getPlayerChoice.charAt(0);
-    let realPlayerChoice = firstLetter.toUpperCase() + playerChoice.slice(1);
-    return realPlayerChoice;
-};
-
 function getComputerChoice() {
-    let choices = ["Rock",
+    let options = ["Rock",
         "Paper",
         "Scissor"
     ];
-    let randomChoice = Math.floor(Math.random() * choices.length);
-    return choices[randomChoice];
+    let randomChoice = Math.floor(Math.random() * options.length);
+    return options[randomChoice];
 };
 
-function playRound(realPlayerChoice, computerChoice) {
-    if (realPlayerChoice === "Rock" && computerChoice === "Paper") {
+function getPlayerChoice() {
+    const playerRock = document.querySelector('#playerRock');
+    playerRock.addEventListener('click', function(e) {
+        return 'Rock'
+    });
+    return playerRock
+}
+
+let choices = [getComputerChoice(), getPlayerChoice()]
+
+const botRock = document.querySelector('#botRock');
+const botPaper = document.querySelector('#botPaper');
+const botScissor = document.querySelector('#botScissor');
+
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice === "Rock" && computerChoice === "Paper") {
         return "You Lose! Paper Beats Rock";
-    } else if (realPlayerChoice === "Rock" && computerChoice === "Scissor") {
+    } else if (playerChoice === "Rock" && computerChoice === "Scissor") {
         return "You Win! Rock beats Scissor";
-    } else if (realPlayerChoice === "Paper" && computerChoice === "Scissor") {
+    } else if (playerChoice === "Paper" && computerChoice === "Scissor") {
         return "You Lose! Scicssor beats Paper";
-    } else if (realPlayerChoice === "Paper" && computerChoice === "Rock") {
+    } else if (playerChoice === "Paper" && computerChoice === "Rock") {
         return "You Win! Paper beats Rock";
-    } else if (realPlayerChoice === "Scissor" && computerChoice === "Rock") {
+    } else if (playerChoice === "Scissor" && computerChoice === "Rock") {
         return "You Lose! Rock beats Scissor";
-    } else if (realPlayerChoice === "Scissor" && computerChoice === "Paper") {
+    } else if (playerChoice === "Scissor" && computerChoice === "Paper") {
         return "You Win! Scissor beats Paper";
-    } else if (realPlayerChoice === computerChoice) {
+    } else if (playerChoice === computerChoice) {
         return "It's a Tie! No one wins";
     } else {
         return "Something went wrong";
     };
 };
 
-let rounds = 0;
+console.log(getPlayerChoice())
+    /*let rounds = 0;
 
-function game() {
-    for (let i = 0; i <= 4; i++) {
-        round++;
-        console.log(playRound(getPlayerChoice(), getComputerChoice()));
-    }
-};
-
-game();
+    function game() {
+        for (let i = 0; i <= 4; i++) {
+            round++;
+            console.log(playRound(getPlayerChoice(), getComputerChoice()));
+        }
+    };*/
